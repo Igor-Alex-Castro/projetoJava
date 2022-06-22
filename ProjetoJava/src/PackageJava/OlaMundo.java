258ef9311a88585a131d3fc911e0f7e06abdee67
igor alex <igoralex8701@gmail.com>
2022-06-21 23:20:59 -0300

    Criação do projeto

---
+++ ProjetoJava/src/PackageJava/OlaMundo.java
@@ -0,0 +1,11 @@
+package PackageJava;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Ola, mundo");
+		System.out.println("Este é o meu primero programa");
+		
+	}
+
+}
